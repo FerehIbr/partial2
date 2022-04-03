d603d43ced3eb2d2289544bf4fe71f43b4ddeb04
ferehibrahimli123@outlook.com <ferehibrahimli123@outlook.com>
2022-04-03 23:11:24 +0400

    first

---
+++ Pustok/wwwroot/js/main.js
@@ -0,0 +1,17 @@
+﻿
+$(document).ready(function () {
+    $(document).on("click", "#productdetailId", function (e) {
+        e.preventDefault();
+        let url = $(this).attr("href");
+        console.log(url);
+
+        fetch(url).then(res => { return res.text }).then(data =>
+        {
+            /* console.log(data)*/
+            $("quickModal .product-details-modal").append.(data);
+
+
+        });
+        $("quickModal").model("show");
+    })
+})
